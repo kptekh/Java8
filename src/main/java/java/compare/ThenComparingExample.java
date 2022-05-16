b3005a3f7988249381e2d866c0932c10f04732b5
karna <kptekh@gmail.com>
2022-05-16 22:49:37 +0530

    ThenCompare example

--- src/main/java/java/compare/ThenComparingExample.java
+++ src/main/java/java/compare/ThenComparingExample.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ComparingTwoOrMoreAttributes {
+public class ThenComparingExample {
     public static void main(String[] args) {
         Employee e1 = new Employee(1,"Mark",46,"Computers");
         Employee e2 = new Employee(2,"John",65,"Maths");
@@ -15,5 +15,10 @@ public class ComparingTwoOrMoreAttributes {
         List<Employee> employeeList = Arrays.asList(e1,e2,e3,e4,e5);
         List<Employee> afterCompEmpList = employeeList.stream().sorted(Comparator.comparing(Employee::getMarks).reversed().thenComparing(Employee::getName)).collect(Collectors.toList());
         afterCompEmpList.forEach(System.out::println);
+        System.out.println();
+        Comparator<Employee> marksComp = Comparator.comparing(Employee::getMarks);
+        Comparator<Employee> nameComp = Comparator.comparing(Employee::getName);
+        List<Employee> twoCompsList = employeeList.stream().sorted(marksComp.thenComparing(nameComp).reversed()).collect(Collectors.toList());
+        twoCompsList.forEach(System.out::println);
     }
 }
